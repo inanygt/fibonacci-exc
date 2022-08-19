068d68ff3541ec42d4735c59e1e4ae2cab567588
inan yigit <inanygt@hotmail.com>
2022-08-19 10:28:24 +0200

    Created fibo algorythm

--- fibonacci.js
+++ fibonacci.js
@@ -1,6 +1,32 @@
-const fibonacci = function() {
-
+const fibonacci = function (n) {
+   if (n < 0) {
+      return "OOPS";
+   }
+   let a = 0; 
+   let b = 1;
+   let sum; 
+   for (let i = 2; i <= n; i++) {
+      sum = a + b;
+      a = b;
+      b = sum;
+   }
+   return sum;
 };
 
 // Do not edit below this line
 module.exports = fibonacci;
+
+
+// fibonacci = 
+
+// 0 + 1 = 1
+// 1 + 2 = 3
+// 2 + 3 = 5
+// 3 + 5 = 8
+// 5 + 8 = 13
+// 8 + 13 = 21
+
+// npm test fibonacci.spec
+
+
+
